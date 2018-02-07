9bbbaed663e5e88e72ccef79c53db32dcc3fcd29
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-02-07 14:22:21 -0800

    Create /recipes endpoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // we'll use morgan to log the HTTP layer
 const morgan = require('morgan');
-// we'll use body-parser's json() method to 
+// we'll use body-parser's json() method to
 // parse JSON data sent in requests to this app
 const bodyParser = require('body-parser');
 
@@ -17,7 +17,7 @@ const app = express();
 app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
-// so there's some data to look at. Note that 
+// so there's some data to look at. Note that
 // normally you wouldn't do this. Usually your
 // server will simply expose the state of the
 // underlying database.
@@ -29,6 +29,15 @@ ShoppingList.create('peppers', 4);
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+const {Recipes} = require('./models');
+
+Recipes.create('mac and cheese', 'noodles, milk, cheese, water');
+Recipes.create('oatmeal', 'oates, water, brown sugar');
+
+app.get('/recipes', (req, res) => {
+    res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
